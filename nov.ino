c1259025c57be8d2c1735f17a752e86fb7fda971
* <mathewkiprop17@gmail.com>
2021-01-07 15:07:01 +0300

    First commit

---
+++ nov.ino
@@ -0,0 +1,26 @@
+/*
+
+*/
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(4,5);
+void setup() {
+    mySerial.begin(9600);
+    Serial.begin(9600);
+    
+}
+
+void loop() {
+ Serial.print(0x5, BIN);
+ delay(1);
+ Serial.print(0x9, BIN);
+ delay(1);
+ Serial.print(0x11, BIN);
+ delay(3);
+ Serial.print(0x4,BIN);
+ delay(1);
+ Serial.print(0x8, BIN);
+ delay(1);
+ Serial.print(0xA, BIN);
+ delay(2);
+ 
+}
